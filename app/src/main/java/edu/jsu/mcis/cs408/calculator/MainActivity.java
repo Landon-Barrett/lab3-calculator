72b20b0a958ef7cbdba3ec8eb28df191fc1a8ea2
Landon Barrett <lbarrett@stu.jsu.edu>
2024-02-14 12:13:36 -0600

    Final Commit PartB Branch.

--- app/src/main/java/edu/jsu/mcis/cs408/calculator/MainActivity.java
+++ app/src/main/java/edu/jsu/mcis/cs408/calculator/MainActivity.java
@@ -5,8 +5,10 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.constraintlayout.widget.Guideline;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -16,13 +18,14 @@ import android.widget.Toolbar;
 import edu.jsu.mcis.cs408.calculator.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
     private ActivityMainBinding binding;
-    private String TAG = "ACTIVITY_MAIN";
     private static final int KEYS_HEIGHT = 4;
     private static final int KEYS_WIDTH = 5;
+    private String[][] btnText;
+    private String[][] btnTags;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
@@ -33,14 +36,11 @@ public class MainActivity extends AppCompatActivity {
     private void initLayout() {
 
         ConstraintLayout layout = binding.layout;
+        ConstraintSet set = new ConstraintSet();
+        set.clone(layout);
 
         int[][] horizontals = new int[KEYS_HEIGHT][KEYS_WIDTH];
         int[][] verticals = new int[KEYS_WIDTH][KEYS_HEIGHT];
-
-        int[] btnIds = new int[KEYS_HEIGHT];
-
-        ConstraintSet set = new ConstraintSet();
-        set.clone(layout);
 
         Guideline north = binding.guideNorth;
         Guideline south = binding.guideSouth;
@@ -48,23 +48,20 @@ public class MainActivity extends AppCompatActivity {
         Guideline west = binding.guideWest;
 
         int id = View.generateViewId();
-        TextView tv = new TextView(this);
-        tv.setId(id);
-        tv.setText("this is a text view ");
-        tv.setTextSize(24);
-        layout.addView(tv);
+        TextView output = new TextView(this);
+        output.setId(id);
+        output.setTag(getResources().getString(R.string.placeHolderNumTag));
+        output.setText(getResources().getString(R.string.placeHolderNum));
+        output.setTextSize(48);
+        output.setGravity(Gravity.RIGHT);
 
-        //set.connect(tv.getId(), ConstraintSet.LEFT, west.getId(), ConstraintSet.LEFT, 0);
-        //set.connect(tv.getId(), ConstraintSet.RIGHT, east.getId(), ConstraintSet.RIGHT, 0);
-        //set.connect(tv.getId(), ConstraintSet.TOP, north.getId(), ConstraintSet.TOP, 0);
-        //set.connect(tv.getId(), ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM, 0);
+        layout.addView(output);
+        ViewGroup.LayoutParams params = output.getLayoutParams();
 
-
-        String[][] btnText = getButtonText();
+        btnText = getButtonText();
+        btnTags = getButtonTags();
 
         for(int i = 0; i < KEYS_WIDTH; i++) {
-
-            btnIds = new int[KEYS_HEIGHT];
 
             for (int j = 0; j < KEYS_HEIGHT; j++) {
 
@@ -73,19 +70,19 @@ public class MainActivity extends AppCompatActivity {
 
                 btn.setId(id);
                 btn.setText(btnText[i][j]);
+                btn.setTag(btnTags[i][j]);
                 btn.setTextSize(24);
                 layout.addView(btn);
-                btnIds[j] = id;
+
                 horizontals[j][i] = id;
+                verticals[i][j] = id;
             }
 
-            verticals[i] = btnIds;
         }
-
 
         for(int i = 0; i < KEYS_WIDTH; i++) {
 
-            set.createVerticalChain(tv.getId(), ConstraintSet.BOTTOM, south.getId(),
+            set.createVerticalChain(output.getId(), ConstraintSet.BOTTOM, south.getId(),
                     ConstraintSet.BOTTOM, verticals[i], null, ConstraintSet.CHAIN_PACKED);
         }
 
@@ -95,7 +92,14 @@ public class MainActivity extends AppCompatActivity {
                     ConstraintSet.RIGHT, horizontals[i], null, ConstraintSet.CHAIN_PACKED);
         }
 
+        set.connect(output.getId(), ConstraintSet.LEFT, west.getId(), ConstraintSet.LEFT, 0);
+        set.connect(output.getId(), ConstraintSet.RIGHT, east.getId(), ConstraintSet.RIGHT, 0);
+        set.connect(output.getId(), ConstraintSet.TOP, north.getId(), ConstraintSet.TOP, 0);
+        set.connect(output.getId(), ConstraintSet.BOTTOM, (horizontals[0][2]), ConstraintSet.TOP, 0);
         set.applyTo(layout);
+
+        params.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+        output.setLayoutParams(params);
     }
 
     public String[][] getButtonText() {
@@ -109,4 +113,17 @@ public class MainActivity extends AppCompatActivity {
 
         return btnText;
     }
+
+    public String[][] getButtonTags() {
+
+        String[] column1 = getResources().getStringArray(R.array.buttonsColumn1Tags);
+        String[] column2 = getResources().getStringArray(R.array.buttonsColumn2Tags);
+        String[] column3 = getResources().getStringArray(R.array.buttonsColumn3Tags);
+        String[] column4 = getResources().getStringArray(R.array.buttonsColumn4Tags);
+        String[] column5 = getResources().getStringArray(R.array.buttonsColumn5Tags);
+        String[][] btnTags = {column1, column2, column3, column4, column5};
+
+        return btnTags;
+    }
+
 }
